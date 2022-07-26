c9578278ba1b4d914e5515f0ce2209b96e1cff35
yiran <yiran.huang@momenta.ai>
2022-07-26 15:53:46 +0800

    fix(custom): jire550/questionnaire
    添加配置项

--- .cz-config.js
+++ .cz-config.js
@@ -19,13 +19,13 @@ module.exports = {
     messages: {
       type: '请选择提交类型:',
       customScope: '请输入修改范围(可选):',
-      subject: '请简要描述提交(必填):',
+      subject: '[jira-xxx/workitem]:',
       body: '请输入详细描述(可选):',
       footer: '请输入要关闭的issue(可选):',
       confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
     },
     // 跳过问题
-    skipQuestions: ['body', 'footer'],
+    skipQuestions: ['footer'],
     // subject文字长度默认是72
     subjectLimit: 72
   }
