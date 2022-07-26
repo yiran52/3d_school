00901dac268517bab16c9fd2471eb244adb59019
yiran <yiran.huang@momenta.ai>
2022-07-26 17:01:06 +0800

    fix: 01

--- commitlint.config.js
+++ commitlint.config.js
@@ -21,7 +21,10 @@ module.exports = {
         ]
       ],
       // subject 大小写不做校验
-      'subject-case': [2,'always',[
+      'subject-case': [
+        2,
+        'always'
+        ,[
         'jire',
         'yiran'
       ]]
