f54d8dc4d5b4318a4303cb15eecef9826f2ec91f
yiran <yiran.huang@momenta.ai>
2022-08-06 14:46:26 +0800

    feat(custom): 111
    111

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,10 @@ function App() {
     return (
        <>
        <Home/>
-<<<<<<< HEAD
        <h1>22r333</h1>
        <span>555</span>
-=======
        <h1>22r3d33</h1>
        <h1>22r3d3344444</h1>
->>>>>>> test
        </>
     )}
 export default App
